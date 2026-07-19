de30c5e87dc442df33998d4deed451930e226a0d
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2026-07-19 13:49:35 -0500

    first commit

---
+++ Proyecto_springboot/gestionbodega/src/main/java/com/proyectospring/gestionbodega/GestionbodegaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectospring.gestionbodega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbodegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionbodegaApplication.class, args);
+	}
+
+}
---
+++ Proyecto_springboot/gestionbodega/src/test/java/com/proyectospring/gestionbodega/GestionbodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectospring.gestionbodega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionbodegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

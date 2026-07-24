890a9dc138ca6c257c88133e0833b45287aa5f6a
shirlyk89 <226595573+shirlyk89@users.noreply.github.com>
2026-07-23 23:48:45 -0500

    fix: pruebas endpoints entrada, salida , transferencia

--- Proyecto_springboot/gestionbodega/src/main/java/com/proyectospring/gestionbodega/controllers/MovimientoController.java
+++ Proyecto_springboot/gestionbodega/src/main/java/com/proyectospring/gestionbodega/controllers/MovimientoController.java
@@ -1,8 +1,10 @@
 package com.proyectospring.gestionbodega.controllers;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyectospring.gestionbodega.entities.Movimiento;
@@ -40,6 +43,16 @@ public class MovimientoController {
            
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'EMPLEADO')")
+    @GetMapping("/rango")
+    public ResponseEntity<List<Movimiento>> obtenerPorRangoDeFechas(
+            @RequestParam("fechaInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaInicio,
+            @RequestParam("fechaFin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaFin) {
+        
+        List<Movimiento> movimientos = movimientoService.buscarPorRangoDeFechas(fechaInicio, fechaFin);
+        return ResponseEntity.ok(movimientos);
     }
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'EMPLEADO')")
